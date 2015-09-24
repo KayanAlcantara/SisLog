49932e4cfddcccf5045bf61c0b8a6815686fdc5d
Diego <Diego@Diego-PC>
2015-09-24 13:08:03 -0300

    comit para o pacote de conexão com o BD

---
+++ PRODUTO/src/SISLOGBD/conectaBD.java
@@ -0,0 +1,29 @@
+package SISLOGBD;
+
+//conexão com o banco de dados SGE
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class conectaBD {
+    private final Connection connection; 
+    
+    public conectaBD() throws ClassNotFoundException, SQLException{
+         Class.forName("com.mysql.jdbc.Driver");
+         connection = DriverManager.getConnection("jdbc:mysql://localhost/SGE","root","1234");
+         System.out.println("BANCO CONECTADO COM SUCESSO!!");
+         
+     }
+    
+    
+    public static void main(String[] args) {
+        try{
+            conectaBD conectaBD = new conectaBD();
+        }
+        catch(ClassNotFoundException | SQLException e){
+            System.out.println("NÃO FOI POSSIVEL ESTABELECER CONEXÇÃO");
+        }
+    }
+
+}
